f0be9a2b976d0b56919d420515af08035a4324cc
Simo56a0 <abahosimon003@gmail.com>
2025-10-25 22:17:59 +0300

    deploy demo

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@*(u+vs!eb2+19fpx&wd$aio+0jwktjjpk+z)i+d+jb65@b(l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-backend-uq75.onrender.com']
 
 
 # Application definition
